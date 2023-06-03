12b13b96da7709f8874a629665fc60c5d93d87fd
Amy Nagle <kabi-git@openmuffin.com>
2023-06-03 19:59:16 -0400

    Update message.go

--- message.go
+++ message.go
@@ -246,8 +246,8 @@ type MessageSend struct {
 // is also where you should get the instance from.
 type MessageEdit struct {
 	Content         *string                 `json:"content,omitempty"`
-	Components      []MessageComponent      `json:"components"`
-	Embeds          []*MessageEmbed         `json:"embeds"`
+	Components      []MessageComponent      `json:"components,omitempty"`
+	Embeds          []*MessageEmbed         `json:"embeds,omitempty"`
 	AllowedMentions *MessageAllowedMentions `json:"allowed_mentions,omitempty"`
 	Flags           MessageFlags            `json:"flags,omitempty"`
 	// Files to append to the message
